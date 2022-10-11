29742e76faabcbb35ce0d304c68c6ad3d3c8c181
SkyFalI <alewaflamaster@gmail.com>
2022-10-11 22:29:07 +0800

    add choice editingmode

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -15,31 +15,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1 {
+namespace WpfApp1
+{
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
         public ColorRGB mcolor { get; set; }
         public Color clr { get; set; }
-        public MainWindow () {
+        public MainWindow()
+        {
             InitializeComponent();
             mcolor = new ColorRGB();
             mcolor.R = mcolor.B = mcolor.G = 0;
 
         }
         // Очистка доски
-        private void ClearCanvas (object sender, RoutedEventArgs e) {
+        private void ClearCanvas(object sender, RoutedEventArgs e)
+        {
             InkCanvas1.Strokes.Clear();
         }
-        
+
         // Закрытие приложения
-        private void CloseApp (object sender, RoutedEventArgs e) {
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
-        
+
         // Меняем цвет при изменении значенний у слайдера
-        private void ColorChanged (object sender, RoutedPropertyChangedEventArgs<double> e) {
+        private void ColorChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
             mcolor.R = Convert.ToByte(ColorRed.Value);
             mcolor.G = Convert.ToByte(ColorGreen.Value);
             mcolor.B = Convert.ToByte(ColorBlue.Value);
@@ -50,13 +56,13 @@ namespace WpfApp1 {
         }
 
         // Сохраняем свое творчество
-        private void SaveCanvas(object sender, RoutedEventArgs e) {
-            
+        private void SaveCanvas(object sender, RoutedEventArgs e)
+        {
+
             SaveFileDialog SFD = new SaveFileDialog();
             SFD.Filter = "png files|*.png";
             SFD.ShowDialog();
             string Patch = SFD.FileName;
-
             // https://stackoverflow.com/questions/21411878/saving-a-canvas-to-png-c-sharp-wpf
 
             Rect bounds = VisualTreeHelper.GetDescendantBounds(InkCanvas1);
@@ -65,7 +71,8 @@ namespace WpfApp1 {
             RenderTargetBitmap rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
 
             DrawingVisual dv = new DrawingVisual();
-            using (DrawingContext dc = dv.RenderOpen()) {
+            using (DrawingContext dc = dv.RenderOpen())
+            {
                 VisualBrush vb = new VisualBrush(InkCanvas1);
                 dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
             }
@@ -74,17 +81,18 @@ namespace WpfApp1 {
 
             BitmapEncoder pngEncoder = new PngBitmapEncoder();
             pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
-            
-            try {
-                MemoryStream ms = new System.IO.MemoryStream(); 
+
+            try
+            {
+                MemoryStream ms = new MemoryStream();
                 pngEncoder.Save(ms);
                 ms.Close();
                 File.WriteAllBytes(Patch, ms.ToArray());
             }
-            catch (Exception err) {
-                MessageBox.Show(err.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            catch (Exception err)
+            {
+                // MessageBox.Show(err.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
         }
 
         // Меняем толщину кисти
@@ -93,12 +101,33 @@ namespace WpfApp1 {
             InkCanvas1.DefaultDrawingAttributes.Width = (int)InkWidth.Value;
             InkCanvas1.DefaultDrawingAttributes.Height = (int)InkWidth.Value;
         }
+        // Выбираем ластик
+        private void ChoiceEraser(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+        // Выбираем кисть
+        private void ChoicePen(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.Ink;
+        }
+        // Выбираем "Выбрать"
+        private void ChoiceSelect(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.Select;
+        }
+        // Временный объект
+        private void ChoiceTemp(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.GestureOnly;
+        }
     }
 
     // Класс для определения цветов
-    public class ColorRGB {
+    public class ColorRGB
+    {
         public byte R { get; set; }
         public byte G { get; set; }
         public byte B { get; set; }
-    } 
+    }
 }
